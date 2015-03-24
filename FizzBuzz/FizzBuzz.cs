7d5342035ad31a88c5073738da909b084621d9ea
matteomanzo <imatteo.manzo@gmail.com>
2015-03-24 16:59:59 +0000

    setup and first test passing

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	public class GameClass
+	{
+		private string shout;
+
+		public void Game (int num)
+		{
+			if (num % 15 == 0)
+			{
+				shout = "FizzBuzz";
+			}
+		}
+
+		public string Shout
+		{
+			get { return shout; }
+		}
+
+	}
+}
+
---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FizzBuzz
+{
+	[TestFixture]
+	public class GameClassTest
+	{
+		[Test]
+		public void Game()
+		{
+			GameClass number = new GameClass();
+			number.Game (15);
+			Assert.AreEqual("FizzBuzz", number.Shout);
+		}
+	}
+}
