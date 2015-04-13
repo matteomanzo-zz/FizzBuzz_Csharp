1d8619eb866f0856562916fa14e0e04ba1c7b79f
Matteo Manzo <imatteo.manzo@gmail.com>
2015-04-13 23:28:08 +0100

    refactored Main method and tested console output

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,9 +6,21 @@ namespace FizzBuzz
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Type a number")
+			Program program = new Program ();
+			int num = program.ReceiveInput ();
+			program.WriteResult (num);
+		}
+
+		public int ReceiveInput()
+		{
+			Console.WriteLine ("Type a number");
 			string input = Console.ReadLine ();
 			int num = Convert.ToInt32 (input);
+			return num;
+		}
+
+		public void WriteResult(int num)
+		{
 			FizzBuzz fizzbuzz = new FizzBuzz ();
 			Console.WriteLine (fizzbuzz.Shout (num));
 		}
---
+++ FizzBuzz/ProgramTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace FizzBuzz
+{
+	[TestFixture ()]
+	public class ProgramTest
+	{
+		Program program;
+
+		[SetUp()]
+		public void Init()
+		{
+			program = new Program();	
+		}
+
+		[Test()]
+		public void ItShouldPrintAnInitialMessageOnScreen()
+		{
+			StringWriter output = new StringWriter();
+			Console.SetOut(output);
+			program.ReceiveInput ();
+			Assert.AreEqual ("Type a number\n", output.ToString ());
+		}
+	}
+}
